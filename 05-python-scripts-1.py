057f582f39218c0176d78192c60ad067e4d8c177
Reysmer Valle <reysmerwvr@gmail.com>
2020-04-26 21:13:28 -0500

    Add python scripts exercise lesson

---
+++ 05-python-scripts-1.py
@@ -0,0 +1,22 @@
+import sys
+
+if len(sys.argv) >= 2:
+  try:
+    numberArgument = int(sys.argv[1])
+  except ValueError:
+    numberArgument = None
+    print("Oops!  That was no valid number.  Try again...")
+  if numberArgument is not None:
+    stringNumber = str(numberArgument)
+    stringNumberLength = len(stringNumber)
+    listOfNumbers = []
+    for index in range(stringNumberLength):
+      formatWeight = stringNumberLength-index
+      firstFormat = "{:<0{f}d}".format(int(stringNumber[index]), f=formatWeight)
+      secondFormat = "{:>0{n}d}".format(int(firstFormat), n=stringNumberLength)
+      listOfNumbers.append(secondFormat)
+    listOfNumbers.reverse()
+    for number in listOfNumbers:
+      print(number)
+else:
+  print('Error: Missing arguments')
