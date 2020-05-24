965b426fc81fd043e57c5f889ae1f0287158a22f
Reysmer Valle <reysmerwvr@gmail.com>
2020-05-23 20:01:32 -0500

    Add python math module exercises

---
+++ 17-python-math-module.py
@@ -0,0 +1,23 @@
+import math
+
+pi = 3.14159
+print(round(pi))
+print(round(3.5))
+print(math.floor(pi))
+print(math.floor(3.5))
+print(math.floor(3.9999))
+print(math.ceil(pi))
+print(math.ceil(3.00001))
+print(abs(-10))
+num_list = [1, 2, 3]
+print(sum(num_list))
+print(math.fsum(num_list))
+num_list = [0.9999999, 1, 2, 3]
+print(sum(num_list))
+print(math.fsum(num_list))
+print(math.trunc(100.123456))
+print(2**3)
+print(math.pow(2, 3))
+print(math.sqrt(9))
+print(math.pi)
+print(math.e)
